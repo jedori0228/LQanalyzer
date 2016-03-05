b05980a46e7569cc75fca3c424d3e286455722ac
Jae Sung Kim <jae.sung.kim@cern.ch>
2016-03-05 16:04:38 +0900

    Add MUON_HN_TRI to GetMuons()

--- LQAnalysis/src/AnalyzerCore.cc
+++ LQAnalysis/src/AnalyzerCore.cc
@@ -126,6 +126,8 @@ std::vector<snu::KMuon> AnalyzerCore::GetMuons(BaseSelection::ID muid, bool keep
   
   else if(muid == BaseSelection::MUON_HN_FAKELOOSE){   eventbase->GetMuonSel()->SelectMuons(muonColl,BaseSelection::MUON_HN_FAKELOOSE, 15., 2.5);}
 
+  else if(muid == BaseSelection::MUON_HN_TRI){ eventbase->GetMuonSel()->SelectMuons(muonColl,BaseSelection::MUON_HN_TRI, 10., 2.5);}
+
   // Veto cut
   else if(muid == BaseSelection::MUON_HN_VETO){   eventbase->GetMuonSel()->SelectMuons(muonColl,BaseSelection::MUON_HN_VETO, 15., 2.5);}
 
