c779e02c25e1fc0073a6b9f9fba4164e4849dda3
Jae Sung Kim <jae.sung.kim@cern.ch>
2016-03-07 22:39:40 +0900

    add user list for trilepton

--- LQRun/txt/list_user_mc.sh
+++ LQRun/txt/list_user_mc.sh
@@ -8,3 +8,4 @@ declare -a example=('WZ_pythia8')
 
 declare -a tmplist=('DY5to50_madgraph' 'TT_scaleup_MCatNLO' '')
 
+declare -a trilep=('DY10to50_MCatNLO' 'DY50plus_MCatNLO' 'DY50plus_madgraph' 'ggHtomm_Powheg' 'singletop_s_MCatNLO' 'singletop_tbar_Powheg' 'singletop_t_Powheg' 'singletop_tbarW_Powheg' 'singletop_tW_Powheg' 'TTG_MCatNLO' 'TT_MCatNLO' 'TT_MG5' 'ttWJetsToLNu_MCatNLO' 'ttWJetsToQQ_MCatNLO' 'ttZToQQ_MCatNLO' 'TT_powheg' 'vhf_Htomm_Powheg' 'WG_lnuG_madgraph' 'WJets_MCatNLO' 'WW_llnn_powheg' 'WW_doublescattering' 'WW_pythia8' 'WZ_lllnu_MCatNLO' 'WZ_llqq_MCatNLO' 'WN_lllnu_powheg' 'WZZ_MCatNLO' 'WZ_pythia8' 'WpWp_madgraph' 'WpWp_qcd_madgraph' 'ZG_llG_MCatNLO' 'ZZ_llnunu_powheg' 'ZZ_llqq_MCatNLO' 'ZZ_llll_MCatNLO' 'ZZ_llll_powheg' 'ZZ_pythia8' 'ttHnobb_Powheg' 'ttHtobb_Powheg')
