e739c29327e2515661bce3b33e7b77eb518bdaf7
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-01-07 15:36:35 +0900

    non-prompt SR run

--- LQRun/HNSubmit/trilepton_mumumu.sh
+++ LQRun/HNSubmit/trilepton_mumumu.sh
@@ -6,6 +6,8 @@ sktree_bkg -a trilepton_mumumu -S DoubleMuon -s SKTree_DiLepSkim -n 100
 sktree_bkg -a trilepton_mumumu -list trilep_bkg -s SKTree_DiLepSkim -n 100
 sktree_bkg -a trilepton_mumumu -list trilep_signal -s SKTree_LeptonSkim -n 100
 sktree_bkg -a trilepton_mumumu_FR_method -S DoubleMuon -s SKTree_DiLepSkim -n 100
+sktree_bkg -a trilepton_mumumu -list trilep_nonprompt_bkg -s SKTree_DiLepSkim -n 100 -o /data2/CAT_SKTreeOutput/JobOutPut/jskim/LQanalyzer/data/output/CAT/trilepton_mumumu/periodBtoG/non_prompt_MC/
+sktree_bkg -a trilepton_mumumu -list trilep_diboson_had -s SKTree_DiLepSkim -n 100 -o /data2/CAT_SKTreeOutput/JobOutPut/jskim/LQanalyzer/data/output/CAT/trilepton_mumumu/periodBtoG/non_prompt_MC/ -userflag diboson_had 
 
 ##### CR #####
 
