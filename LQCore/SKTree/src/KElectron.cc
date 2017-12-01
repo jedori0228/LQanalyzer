125c7db56bb44f2382a5d57953ea0a10a47c0ed0
Jae Sung Kim <jae.sung.kim@cern.ch>
2017-12-01 14:05:16 +0900

    new mva value added

--- LQCore/SKTree/src/KElectron.cc
+++ LQCore/SKTree/src/KElectron.cc
@@ -341,8 +341,10 @@ Bool_t KElectron::PassTrigMVAHNTightv3() const{
 
 Bool_t KElectron::PassTrigMVAHNTightv4() const{
 
-  float mva_cut=0.93;
-  if(fabs(this->SCEta()) > 1.479) mva_cut=0.93;
+  //float mva_cut=0.93;
+  //if(fabs(this->SCEta()) > 1.479) mva_cut=0.93;
+  float mva_cut=0.758;
+  if(fabs(this->SCEta()) > 1.479) mva_cut=0.758;
   else if(fabs(this->SCEta()) > 0.8) mva_cut=0.825;
   else mva_cut=0.9;
 
@@ -430,12 +432,21 @@ Bool_t KElectron::PassTrigMVAHNLoose() const{
   if((fabs(this->SCEta())  < 2.5) &&(fabs(this->SCEta())  > 1.479) && k_mva > 0.37) return true;
 */
 
+/*
   //==== ELECTRON_HN_FAKELOOSEv7 opti
   //==== Iso 0.6 Opti
   //==== Based on light-haevy min diff
   if((fabs(this->SCEta()) < 0.8) && k_mva > -0.35) return true;
   if((fabs(this->SCEta())  > 0.8) &&(fabs(this->SCEta())  < 1.479)  && k_mva > -0.34) return true;
   if((fabs(this->SCEta())  < 2.5) &&(fabs(this->SCEta())  > 1.479) && k_mva > 0.43) return true;
+*/
+
+  //==== ELECTRON_HN_FAKELOOSEv7_2 opti
+  //==== Iso 0.6 Opti
+  //==== Based on light-haevy min diff
+  if((fabs(this->SCEta()) < 0.8) && k_mva > -0.35) return true;
+  if((fabs(this->SCEta())  > 0.8) &&(fabs(this->SCEta())  < 1.479)  && k_mva > -0.34) return true;
+  if((fabs(this->SCEta())  < 2.5) &&(fabs(this->SCEta())  > 1.479) && k_mva > -0.30) return true;
 
 /*
   //==== ELECTRON_HN_FAKELOOSEv2 opti
