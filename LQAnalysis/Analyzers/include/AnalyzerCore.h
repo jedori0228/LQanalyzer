1da6474ee812f631856582a42289713e472fe0ee
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-03-16 17:03:39 +0900

    MET correction update

--- LQAnalysis/Analyzers/include/AnalyzerCore.h
+++ LQAnalysis/Analyzers/include/AnalyzerCore.h
@@ -456,6 +456,17 @@ class AnalyzerCore : public LQCycleBase {
   void JSFillHist(TString suffix, TString histname, float value, float w, float xmin, float xmax, int nbins);
   void JSMakeHistograms(TString suffix, TString hname, int nbins, float xmin, float xmax);
   bool HasLeptonInsideJet(snu::KJet jet, std::vector<snu::KMuon> mus, std::vector<snu::KElectron> els);
+  inline static bool MuonPtComparing(const snu::KMuon& m1, const snu::KMuon& m2){ return (m1.Pt() > m2.Pt()); }
+  inline static bool ElectronPtComparing(const snu::KElectron& e1, const snu::KElectron& e2){ return (e1.Pt() > e2.Pt()); }
+  inline static bool LeptonPtComparing(const KLepton& l1, const KLepton& l2){ return (l1.Pt() > l2.Pt()); }
+
+  void JSCorrectedMETRochester(std::vector<snu::KMuon> muall, double& OrignialMET, double& OriginalMETPhi);
+  void JSCorrectedMETMuon(int sys, std::vector<snu::KMuon> muall, double& OrignialMET, double& OriginalMETPhi);
+  void JSCorrectedMETElectron(int sys, std::vector<snu::KElectron> elall, double& OrignialMET, double& OriginalMETPhi);
+  void JSCorrectedMETFatJet(std::vector<snu::KFatJet> fjets ,double& OrignialMET, double& OriginalMETPhi);
+  void JSCorrectedMETJetInsideFatJet(std::vector<snu::KJet> jets ,double& OrignialMET, double& OriginalMETPhi);
+  void FillLeptonKinematicPlot(std::vector<KLepton> lep, TString suffix, double w);
+  void FillUpDownLeptonKinematicPlot(std::vector<KLepton> lep, TString suffix, double w, double w_err);
 
 };
 #endif
